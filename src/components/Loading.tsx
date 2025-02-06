3e3678818c53a4fe8b1ec269ed6d9ef6f833dc19
manas-agarwal16 <manas.agarwal1604@gmail.com>
2025-02-07 01:06:52 +0530

    Loader update

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,15 +1,34 @@
 import React from "react";
+import { motion, Variants } from "framer-motion";
 
-const Loading = ({width = 10}) => {
+const Loading: React.FC = () => {
+  const dotVariants: Variants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.6,
+        repeat: Infinity,
+        ease: "easeInOut",
+        repeatType: "reverse",
+      },
+    },
+  };
+
   return (
-    <div>
-      return (
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-30 backdrop-blur-[2px]">
-        <div
-          className={`w-${width} h-${width} border-4 border-[#A0522D] border-t-transparent border-solid rounded-full animate-spin`}
-        ></div>
+    <div className="absolute inset-0 z-50 flex justify-center items-center gap-2">
+      <div className="flex space-x-2">
+        {[0, 1, 2].map((dot) => (
+          <motion.div
+            key={dot}
+            className="w-2 h-2 bg-gray-500 rounded-full"
+            variants={dotVariants}
+            initial="hidden"
+            animate="visible"
+            style={{ animationDelay: `${dot * 0.3}s` }}
+          />
+        ))}
       </div>
-      );
     </div>
   );
 };
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import Input from "./Input";
+import Loading from "./Loading";
+import TaskCard from "./TaskCard";
+
+export {Input , Loading , TaskCard};
