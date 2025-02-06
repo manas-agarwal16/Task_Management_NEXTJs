3c7e3780b001bcaa2ad6ffe5e6f3055b1abc46d0
manas-agarwal16 <manas.agarwal1604@gmail.com>
2025-02-07 00:42:51 +0530

    Loading.tsx component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Loading = ({width = 10}) => {
+  return (
+    <div>
+      return (
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-30 backdrop-blur-[2px]">
+        <div
+          className={`w-${width} h-${width} border-4 border-[#A0522D] border-t-transparent border-solid rounded-full animate-spin`}
+        ></div>
+      </div>
+      );
+    </div>
+  );
+};
+
+export default Loading;
